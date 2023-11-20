ee3def87844a08b50a9d4b15685038abd301853b
Shayan Hosseini <popsps17@gmail.com>
2023-11-20 14:48:41 -0500

    sync

---
+++ db/hearts_role.sql
@@ -0,0 +1,2 @@
+INSERT INTO hearts.role (id, name) VALUES (1, 'ADMIN');
+INSERT INTO hearts.role (id, name) VALUES (2, 'PLAYER');
---
+++ db/hearts_stats.sql
@@ -0,0 +1,4 @@
+INSERT INTO hearts.stats (user_id, win, lost, average_placement, points_taken_per_game) VALUES (1, 11, 4, 1.8, 36.06666666666667);
+INSERT INTO hearts.stats (user_id, win, lost, average_placement, points_taken_per_game) VALUES (2, 11, 4, 1.7333333333333334, 34.06666666666667);
+INSERT INTO hearts.stats (user_id, win, lost, average_placement, points_taken_per_game) VALUES (3, 8, 7, 2.2666666666666666, 59.2);
+INSERT INTO hearts.stats (user_id, win, lost, average_placement, points_taken_per_game) VALUES (4, 14, 1, 1.2, 16.933333333333334);
